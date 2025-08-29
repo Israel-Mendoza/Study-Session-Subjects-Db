aba8e9e0b1d7c62b53efc09abb3a9c5f233561ba
Israel Mendoza <xander_irsl@hotmail.com>
2025-08-28 22:53:40 -0600

    Modified table names

--- init.sql
+++ init.sql
@@ -1,19 +1,19 @@
-CREATE TABLE Users (
+CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE Subjects (
+CREATE TABLE subjects (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     is_archived BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE Topics (
+CREATE TABLE topics (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     subject_id INT NOT NULL,
     is_archived BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (subject_id) REFERENCES Subjects(id)
+    FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
